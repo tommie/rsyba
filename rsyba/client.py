724249593a9f8de941e44f94009df6dad38b4747
Tommie Gannert <tommie@gannert.se>
2015-04-19 17:33:05 +0100

    Add retry and progress reporting for the client.

--- rsyba/client.py
+++ rsyba/client.py
@@ -3,28 +3,54 @@ import datetime
 import logging
 import os.path
 import socket
+import subprocess
 import sys
 import tempfile
+import time
 
 from rsyba import rsync
 
 log = logging.getLogger(__name__)
+
+def remote_path(archive):
+    if ':' in archive:
+        return archive.split(':', 1)[1]
+
+    return archive
 
 def main():
     argp = argparse.ArgumentParser(usage='%(prog)s [options] <archive> <local>...')
     argp.add_argument('--bwlimit', metavar='kbps', type=int, help='set transfer bandwidth limit')
     argp.add_argument('-n', '--dry-run', action='store_true', default=False, help='do not do any modifications')
     argp.add_argument('-f', '--filter', metavar='RULE', action='append', default=[], help='add source file filter rule')
-    argp.add_argument('--hostname', metavar='FQDN', default=socket.getfqdn(), help='override hostname [default %(default)s]')
-    argp.add_argument('--max_file_size', metavar='INT[KMG]', default='500M', help='ignore files larger than this')
+    argp.add_argument('--hostname', metavar='FQDN', default=socket.gethostname(), help='override hostname [default %(default)s]')
+    argp.add_argument('--max-file-size', metavar='INT[KMG]', default='1G', help='ignore files larger than this')
+    argp.add_argument('--timeout', metavar='INT', type=int, default=12*60*60, help='set transfer time limit in seconds [default %(default)s]')
     argp.add_argument('archive', nargs=1, help='base URL of remote location')
-    argp.add_argument('local', nargs='+', help='local path with optional archive tree name')
+    argp.add_argument('local', metavar='path[=tree]', nargs='+', help='local path with optional archive tree name')
     args = argp.parse_args()
     args.archive = args.archive[0]
 
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-    ts = datetime.datetime.utcnow()
-    ts = ts.strftime('%Y-%m-%dT%H-%M-%S.%f')
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(levelname).1s%(levelname).1s %(asctime)s %(message)s')
+    ts = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S.%f')
+    start = time.time()
+    nfiles = 0
+
+    if os.isatty(sys.stdout.fileno()):
+        def init_progress():
+            print('Listing files...')
+        def print_progress(ch):
+            print('\033[1A[%d %s] %s %s\033[K' % (nfiles, datetime.timedelta(seconds=time.time() - start), ch.updates, ch.filename))
+            sys.stdout.flush()
+        def end_progress():
+            print('\033[1A[%d %s] Done.\033[K' % (nfiles, datetime.timedelta(seconds=time.time() - start)))
+    else:
+        def init_progress():
+            pass
+        def print_progress(ch):
+            log.debug('%s', ch)
+        def end_progress():
+            pass
 
     for pt in args.local:
         pt = pt.split('=', 1)
@@ -35,24 +61,47 @@ def main():
 
         host_base = '/'.join([args.archive.rstrip('/'), 'upload', tree, args.hostname])
 
-        log.info('rsync starting...')
-        rsync.run(
-            '/'.join([host_base, ts, '']),
-            os.path.abspath(path) + os.sep,
-            archive=True,
-            belimit=args.bwlimit,
-            compress=True,
-            dry_run=args.dry_run,
-            fake_super=True,
-            filter=args.filter,
-            ignore_existing=True,
-            link_dest='/'.join([host_base, 'latest', '']),
-            max_size=args.max_file_size,
-            prune_empty_dirs=True,
-            safe_links=True,
-            temp_dir='/'.join([host_base, 'tmp', '']),
-            timeout=60)
+        log.debug('Starting upload for tree %r...', tree)
+        while True:
+            init_progress()
+            try:
+                it = rsync.run_iter(
+                    '/'.join([host_base, ts, '']),
+                    os.path.abspath(path) + os.sep,
+                    archive=True,
+                    bwlimit=args.bwlimit,
+                    compress=True,
+                    dry_run=args.dry_run,
+                    fake_super=True,
+                    filter=args.filter,
+                    ignore_existing=True,
+                    link_dest=remote_path('/'.join([host_base, 'latest', ''])),
+                    max_size=args.max_file_size,
+                    prune_empty_dirs=True,
+                    safe_links=True,
+                    temp_dir=remote_path('/'.join([host_base, 'tmp', ''])),
+                    timeout=args.timeout,
+                    gen_changes=rsync.FileChange(filename=True, size=True, updates=True, mtime=True))
+                next_progress = time.time()
+                for ch in it:
+                    nfiles += 1
+                    t = time.time()
+                    if next_progress <= t:
+                        print_progress(ch)
+                        while next_progress <= t:
+                            next_progress += 1
+                break
+            except subprocess.CalledProcessError as ex:
+                now = time.time()
+                if now - start >= args.timeout:
+                    raise
+            finally:
+                end_progress()
 
+            log.warning('Upload failed. Retrying in 5 s...')
+            time.sleep(5)
+
+        log.info('Finalizing upload of tree %r after %d files...', tree, nfiles)
         with tempfile.TemporaryDirectory(prefix='rsyba_tmp') as dpath:
             # TODO: latest-up is annoyingly concurrency unsafe. Add lock or don't overwrite a later one.
             os.symlink(ts, os.path.join(dpath, 'latest'))
@@ -64,8 +113,10 @@ def main():
                 dry_run=args.dry_run,
                 links=True,
                 safe_links=True,
-                temp_dir='/'.join([host_base, 'tmp', '']),
+                temp_dir=remote_path('/'.join([host_base, 'tmp', ''])),
                 timeout=60)
+
+    log.info('All done after %s.', datetime.timedelta(seconds=time.time() - start))
 
 if __name__ == '__main__':
     main()
--- rsyba/rsync.py
+++ rsyba/rsync.py
@@ -1,33 +1,160 @@
+import datetime
 import logging
 import os
 import subprocess
+import sys
 
 log = logging.getLogger(__name__)
 
-RSYNC = os.environ.get('RSYBA_RSYNC', 'rsync')
+EXIT_SUCCESS = 0
+EXIT_USAGE = 1
+EXIT_PROTOCOL = 2
+EXIT_SELECT = 3
+EXIT_NOTSUPPORTED = 4
+EXIT_START = 5
+EXIT_LOG = 6
+EXIT_SOCKET = 10
+EXIT_FILE = 11
+EXIT_DATA = 12
+EXIT_DIAG = 13
+EXIT_IPC = 14
+EXIT_INT = 20
+EXIT_WAIT = 21
+EXIT_ALLOC = 22
+EXIT_PARTIAL = 23
+EXIT_VANISHED = 24
+EXIT_MAX_DELETE = 25
+EXIT_IO_TIMEOUT = 30
+EXIT_CONN_TIMEOUT = 35
 
-def run(dest, *srcs, rsync_bin=RSYNC, **kwargs):
+def parse_ts(s):
+    return datetime.datetime.strptime(s, '%Y/%m/%d-%H:%M:%S')
+
+class FileUpdates(object):
+    def __init__(self, s):
+        self.s = s
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.s)
+
+    def __str__(self):
+        return self.s
+
+    @property
+    def update_type(self): return self.s[0]
+    @property
+    def file_type(self): return self.s[1]
+    @property
+    def checksum(self): return self.s[2].replace('c', '-')
+    @property
+    def size(self): return self.s[3].replace('s', '-')
+    @property
+    def mtime(self): return self.s[4].replace('t', '-') # TODO: Handle T
+    @property
+    def perms(self): return self.s[5].replace('p', '-')
+    @property
+    def owner(self): return self.s[6].replace('o', '-')
+    @property
+    def group(self): return self.s[7].replace('g', '-')
+    @property
+    def acl(self): return self.s[9].replace('a', '-')
+    @property
+    def xattr(self): return self.s[10].replace('x', '-')
+
+class FileChange(object):
+    __fields = [
+        'transferred',
+        'posix_perms',
+        'filename',
+        'updates',
+        'size',
+        'mtime',
+        'op',
+        'time',
+        'uid',
+        'gid']
+
+    def __init__(self, **kwargs):
+        for k in self.__fields:
+            setattr(self, k, kwargs.get(k, None))
+
+    def __repr__(self):
+        return '%s(%s)' % (
+            self.__class__.__name__,
+            ', '.join('%s=%r' % (f, getattr(self, f))
+                      for f in self.__fields
+                      if getattr(self, f) is not None))
+
+RSYNC = os.environ.get('RSYBA_RSYNC', 'rsync')
+CHANGE_PREFIX = '$change'
+
+def run(*args, gen_changes=None, **kwargs):
+    if gen_changes is not None:
+        raise TypeError('gen_changes not supported with run(); use run_iter()')
+
+    for _ in run_iter(*args, **kwargs):
+        pass
+
+def run_iter(dest, *srcs, rsync_bin=RSYNC, gen_changes=None, **kwargs):
     kwargs.setdefault('motd', False)
-    kwargs.setdefault('quiet', True)
-    kwargs.setdefault('rsync_path', 'rsyba-server --')
+    #kwargs.setdefault('rsync_path', 'rsyba-server --')
+
+    out_format = []
+    out_fields = []
+    def add_out(fmt, field, parser=lambda x: x):
+        if gen_changes is True or getattr(gen_changes, field):
+            out_format.append(fmt)
+            out_fields.append((field, parser))
+
+    if gen_changes:
+        out_format.append(CHANGE_PREFIX)
+        add_out('%b', 'transferred', int)
+        add_out('%B', 'posix_perms')
+        add_out('%i', 'updates', FileUpdates)
+        add_out('%l', 'size', int)
+        add_out('%M', 'mtime', parse_ts)
+        add_out('%o', 'op')
+        add_out('%t', 'time', parse_ts)
+        add_out('%U', 'uid', int)
+        add_out('%G', 'gid', int)
+        # Always last:
+        add_out('%f', 'filename')
+
+        kwargs['out_format'] = '\t'.join(out_format)
+    else:
+        kwargs.setdefault('quiet', True)
 
     opts = []
     for optl in [_option(k, v) for (k, v) in sorted(kwargs.items())]:
         opts.extend(optl)
 
-    cmd = [RSYNC] + opts + list(srcs) + [dest]
-    with open(os.devnull, 'r') as devnull:
-        p = subprocess.Popen(cmd, stdin=devnull)
-        
+    cmd = [rsync_bin] + opts + list(srcs) + [dest]
+    p = subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE if gen_changes else None)
+
     try:
-        p.communicate()
+        if gen_changes:
+            for item in p.stdout:
+                item = str(item, 'utf-8')
+                if not item.startswith(CHANGE_PREFIX + '\t'):
+                    continue
+
+                parts = item.rstrip().split('\t', len(out_fields) - 1 + 1)[1:]
+                yield FileChange(**{field: func(s) for s, (field, func) in zip(parts, out_fields)})
+
+            p.stdout.close()
+        else:
+            # Wait here to catch exceptions and terminate() if that happens.
+            p.wait()
     except:
         p.terminate()
         raise
     finally:
         p.wait()
         if p.returncode:
-            raise subprocess.CalledProcessError(p.returncode, cmd)
+            # Don't raise CPE if rsync terminated because of SIGINT and we
+            # already have an active exception.
+            if p.returncode != EXIT_INT or sys.exc_info()[0] is None:
+                raise subprocess.CalledProcessError(p.returncode, cmd)
 
 def _option(k, v):
     k = k.replace('_', '-')
@@ -41,7 +168,7 @@ def _option(k, v):
         return ['--no-' + k]
     elif isinstance(v, list):
         return [_option(k, vv) for vv in v]
-    elif v:
-        return ['--%s=%s' % (k, v)]
-    else:
+    elif v is None:
         return []
+    else:
+        return ['--%s=%s' % (k, v)]
